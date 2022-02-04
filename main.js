b6204260d8ead6134d2b23d2eab928285c855722
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2022-02-04 17:22:58 +0900

    dynamic pathcode

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+/**
+ * @params parhCode? http://www.jma.go.jp/bosai/common/const/area.json に記載のあるエリアコード。デフォルトは東京
+ */
 class TokyoWeatherCard extends HTMLElement {
   constructor() {
     super();
-    // シャドウルートを生成
     this.attachShadow({ mode: "open" });
+
+    const pathCode = this.hasAttribute("path-code")
+      ? this.getAttribute("path-code")
+      : "130000";
 
     // (内部の) span 要素を生成
     const wrapper = document.createElement("div");
+    this.wrapper = wrapper;
     wrapper.setAttribute("class", "wrapper");
 
     const cardTitle = document.createElement("h2");
@@ -13,24 +20,43 @@ class TokyoWeatherCard extends HTMLElement {
     wrapper.appendChild(cardTitle);
 
     const cardContent = document.createElement("div");
-    this.fetchAndSetData(cardContent);
+    this.fetchAndSetData(cardContent, pathCode);
     wrapper.appendChild(cardContent);
 
-    this.shadowRoot.append(wrapper);
+    const style = document.createElement("style");
+    style.textContent = `
+    .wrapper {
+      background-color: #cccccc;
+      color: #111111;
+      padding: 1rem;
+    }
+    .wrapper.alert {
+      background-color: #cc5555;
+      color: #111111;
+      padding: 1rem;
+    }
+    .wrapper.advisory {
+      background-color: #cccc55;
+      color: #111111;
+      padding: 1rem;
+    }
+    `;
+
+    this.shadowRoot.append(wrapper, style);
   }
 
-  async fetchAndSetData(element) {
-    const fetchWarning = async (areaCode) => {
+  async fetchAndSetData(element, pathCode) {
+    const fetchWarning = async () => {
       const result = await (
         await fetch(
-          `https://www.jma.go.jp/bosai/warning/data/warning/${areaCode}.json`
+          `https://www.jma.go.jp/bosai/warning/data/warning/${pathCode}.json`
         )
       ).json();
-      console.log(JSON.stringify(result, undefined, 2));
       return result;
     };
+    // this.wrapper.classList.add("alert");
 
-    const dataFromGoJp = await fetchWarning("130000");
+    const dataFromGoJp = await fetchWarning();
 
     element.innerText = dataFromGoJp["headlineText"];
   }
